fd5c16836bdd1007c5cc0f6f9986280afd49c34c
saleemAbed99 <saleemabed15@gmail.com>
2022-04-14 01:18:21 +0300

    basics

---
+++ src/app/Components/tasks/tasks.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { TaskService } from './../../services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task): void {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => task.id !== t.id))
+      );
+  }
+
+  toggleReminder(task: Task): void {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+    console.log(task.reminder);
+  }
+}
